78a9dc13f9f94fd23a2cb3587aa649dbe228e061
JFFBMA <jorgef.figueroa@gmail.com>
2024-07-03 20:50:40 -0500

    first commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "./Login.css";
-import { Button, Form, FormGroup, Alert } from 'react-bootstrap';
+import { Button, Form, FormGroup, Alert, Container, Row, Col } from 'react-bootstrap';
 import { iAxios } from "../Services/Interceptors"
 
 function Login() {
@@ -39,35 +39,40 @@ function Login() {
 
     return (
         <>
-            <Form noValidate onSubmit={handleSubmit} validated={validated}>
-                <FormGroup controlId='idEmail'>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control name='email' type='email' placeholder='Correo Electronico' required onChange={(e) => setEmail(e.target.value)}/>
-                    <Form.Control.Feedback type='invalid'>
-                        Correo Electronico no valido
-                    </Form.Control.Feedback>
-                </FormGroup>
-                <FormGroup controlId='idPassword'>
-                    <Form.Label>Contraseña</Form.Label>
-                    <Form.Control name='password' type='password' placeholder='Contraseña' required onChange={(e) => setPassword(e.target.value)}/>
-                    <Form.Control.Feedback type='invalid'>
-                        Contraseña no valida
-                    </Form.Control.Feedback>
-                </FormGroup>
-                <FormGroup>
-                    <Form.Label>Tratamiento de datos</Form.Label>
-                    <Form.Check type='radio' name='td' value={true} label="Si" onChange={(e) => setAcceptance(e.target.value)}/>
-                    <Form.Check type='radio' name='td' value={false} label="No" onChange={(e) => setAcceptance(e.target.value)}/>
-                </FormGroup>
-                <Button variant='warning' type='submit'>Enviar</Button>
-            </Form>
-            {error && <Alert variant="danger">{error}</Alert>}
-            {respuesta !== null && (  // Mostrar solo si respuesta no es null
-                <div>
-                    <h2>Respuesta:</h2>
-                    <p>{JSON.stringify(respuesta)}</p>
-                </div>
-            )}
+            <Container>
+                <Row>
+                    <Col><Form noValidate onSubmit={handleSubmit} validated={validated}>
+                        <FormGroup controlId='idEmail'>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control name='email' type='email' placeholder='Correo Electronico' required onChange={(e) => setEmail(e.target.value)} />
+                            <Form.Control.Feedback type='invalid'>
+                                Correo Electronico no valido
+                            </Form.Control.Feedback>
+                        </FormGroup>
+                        <FormGroup controlId='idPassword'>
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control name='password' type='password' placeholder='Contraseña' required onChange={(e) => setPassword(e.target.value)} />
+                            <Form.Control.Feedback type='invalid'>
+                                Contraseña no valida
+                            </Form.Control.Feedback>
+                        </FormGroup>
+                        <FormGroup>
+                            <Form.Label>Tratamiento de datos</Form.Label>
+                            <Form.Check type='radio' name='td' value={true} label="Si" onChange={(e) => setAcceptance(e.target.value)} />
+                            <Form.Check type='radio' name='td' value={false} label="No" onChange={(e) => setAcceptance(e.target.value)} />
+                        </FormGroup>
+                        <Button variant='warning' type='submit'>Enviar</Button>
+                    </Form>
+                        {error && <Alert variant="danger">{error}</Alert>}
+                        {respuesta !== null && (  // Mostrar solo si respuesta no es null
+                            <div>
+                                <h2>Respuesta:</h2>
+                                <p>{JSON.stringify(respuesta)}</p>
+                            </div>
+                        )}</Col>
+                </Row>
+            </Container>
+
         </>
     )
 }
